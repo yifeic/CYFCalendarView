c38ba28af2bffd9d9ea706b77c14319edf4670df
yifeic <yifei.chen@outlook.com>
2015-06-25 17:46:22 -0400

    CYFCalendarDraggableViewTouchArea

--- Pod/Classes/CYFCalendarDraggableView.m
+++ Pod/Classes/CYFCalendarDraggableView.m
@@ -8,11 +8,19 @@
 
 #import "CYFCalendarDraggableView.h"
 
+typedef NS_ENUM(NSUInteger, CYFCalendarDraggableViewTouchArea) {
+    CYFCalendarDraggableViewTouchAreaContent,
+    CYFCalendarDraggableViewTouchAreaTopHandle,
+    CYFCalendarDraggableViewTouchAreaBottomHandle,
+};
+
 @interface CYFCalendarDraggableView ()
 @property (nonatomic, strong, readonly) UILongPressGestureRecognizer *panGestureRecognizer;
 @property (nonatomic, strong, readonly) CYFCalendarDraggableViewDragBlock onDrag;
 @property (nonatomic, readwrite) CGPoint dragBeginPointInSuperview;
 @property (nonatomic, readwrite) CGPoint dragBeginCenter;
+@property (nonatomic, weak) UIView *topResizeHandle;
+@property (nonatomic) CYFCalendarDraggableViewTouchArea touchBeginArea;
 @end
 
 @implementation CYFCalendarDraggableView
@@ -25,20 +33,23 @@
         _onDrag = onDrag;
         _contentViewInsets = UIEdgeInsetsMake(10, 0, 0, 0);
         
-        [self addSubview:view];
-        view.translatesAutoresizingMaskIntoConstraints = NO;
-        NSDictionary *viewDict = NSDictionaryOfVariableBindings(view);
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[view]|" options:0 metrics:nil views:viewDict]];
-        NSString *contentViewVFormat = [NSString stringWithFormat:@"V:|-%f-[view]-%f-|", self.contentViewInsets.top, self.contentViewInsets.bottom];
-        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:contentViewVFormat options:0 metrics:nil views:viewDict]];
-        
         _panGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(drag:)];
         _panGestureRecognizer.minimumPressDuration = 0;
         [self addGestureRecognizer:self.panGestureRecognizer];
         
+        [self setupContentView:view];
         [self setupResizeHandle];
     }
     return self;
+}
+
+- (void)setupContentView:(UIView *)view {
+    [self addSubview:view];
+    view.translatesAutoresizingMaskIntoConstraints = NO;
+    NSDictionary *viewDict = NSDictionaryOfVariableBindings(view);
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|[view]|" options:0 metrics:nil views:viewDict]];
+    NSString *contentViewVFormat = [NSString stringWithFormat:@"V:|-%f-[view]-%f-|", self.contentViewInsets.top, self.contentViewInsets.bottom];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:contentViewVFormat options:0 metrics:nil views:viewDict]];
 }
 
 - (void)setupResizeHandle {
@@ -49,10 +60,18 @@
     NSDictionary *viewDict = NSDictionaryOfVariableBindings(handle);
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-10-[handle(20)]" options:0 metrics:nil views:viewDict]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[handle(20)]" options:0 metrics:nil views:viewDict]];
+    self.topResizeHandle = handle;
 }
 
 - (void)drag:(UILongPressGestureRecognizer *)gesture {
     if (gesture.state == UIGestureRecognizerStateBegan) {
+        if ([self.topResizeHandle pointInside:[gesture locationInView:self.topResizeHandle] withEvent:nil]) {
+            self.touchBeginArea = CYFCalendarDraggableViewTouchAreaTopHandle;
+        }
+        else {
+            self.touchBeginArea = CYFCalendarDraggableViewTouchAreaContent;
+        }
+        
         self.dragBeginPointInSuperview = [gesture locationInView:self.superview];
         self.dragBeginCenter = self.center;
     }
